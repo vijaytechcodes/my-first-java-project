a53f0abcf21232d4a2386ff6fdcc1e3ff3730eca
vijaytechcodes <vijayidk27@gmail.com>
2026-08-24 20:48:52 +0530

    Add files via upload

---
+++ rivise.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class rivise {
+   
+   public static void main(String[] args) {
+   Scanner sc = new Scanner (System.in);  
+//     int a = 34;
+ //    int b = 34;
+ //    int sum = a + b ;
+ //    System.out.println(sum);
+   System.out.println("taking input from user ");   
+   System.out.println("enter your first number ");
+   //    int a = sc.nextInt();
+     float a = sc.nextFloat();
+   System.out.println("enter your second number ");    
+     //  int b =sc.nextInt();
+     float b = sc.nextFloat();
+    float sum = a + b ;
+    System.out.println(sum);
+     
+   }
+}
